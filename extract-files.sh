11ffe5bb68342c2d8f052ef05825c7aa4b40047d
Nick Me <crtjstr@gmail.com>
2012-04-28 21:56:38 -0400

    another typo

--- extract-files.sh
+++ extract-files.sh
@@ -307,7 +307,7 @@ adb pull /system/lib/libaudio.so ../../../out/target/product/galaxys4g/obj/lib/l
 mkdir ../../../out/target/product/galaxys4g/system/lib/
 adb pull /system/lib/libaudio.so ../../../out/target/product/galaxys4g/system/lib/libaudio.so
 
-adb pull /system/lib/libaudiopolicy.so ../../../proprietary/libaudiopolicy.so
+adb pull /system/lib/libaudiopolicy.so ../../../proprietary/lib/libaudiopolicy.so
 adb pull /system/lib/libaudiopolicy.so ../../../out/target/product/galaxys4g/obj/lib/libaudiopolicy.so
 
 ./setup-makefiles.sh
